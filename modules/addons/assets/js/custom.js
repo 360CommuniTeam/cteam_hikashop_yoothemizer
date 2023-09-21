d1ec55ea0eccce528be7373bd0b2de295de5939e
Woem <l.stoller@outlook.fr>
2023-09-21 12:58:31 +0200

    Add support for J4 Password field

--- modules/addons/assets/js/custom.js
+++ modules/addons/assets/js/custom.js
@@ -5,8 +5,25 @@
   
     window.addEventListener("load", function() {
   
+      var passwordToggleElements = document.querySelectorAll('.uk-input-password-toggle');
       
-  
+      passwordToggleElements.forEach((elm) => {
+        elm.addEventListener('click', function (e) {
+          e.preventDefault();
+          
+          if(elm.previousElementSibling.getAttribute('type') == 'password') {
+            elm.previousElementSibling.setAttribute('type', 'text');
+            elm.setAttribute('uk-icon', 'icon: eye-slash');
+          } else {
+            elm.previousElementSibling.setAttribute('type', 'password');
+            elm.setAttribute('uk-icon', 'icon: eye');
+          }
+
+
+          return false;
+        });
+      })
+
     });
   
   })();
